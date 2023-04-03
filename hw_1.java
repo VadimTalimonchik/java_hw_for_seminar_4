b70d090fea8f5937f5a9398223c4fb235c7520bf
VadimTalimonchik <tvadmins@gmail.com>
2023-04-03 10:13:53 +0300

    Задача 1

--- hw_1.java
+++ hw_1.java
@@ -1,8 +1,41 @@
 // Задача 1.
 // Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернёет «перевёрнутый» список.
 
-public class hw_1 {
-    public static void main(String[] args) {
+import java.util.LinkedList;
 
+public class hw_1 {
+    public static LinkedList<Object> fillList(){
+        LinkedList<Object> list = new LinkedList<>();
+        list.add("Начало");
+        list.add("Один");
+        list.add("Два");
+        list.add(3);
+        list.add("Четыре");
+        list.add("Пять");
+        list.add("Конец");
+        return list;
+    }
+    public static void printList(LinkedList<Object> list){
+        for(Object el: list){
+            System.out.printf(el + " ");
+        }
+    }
+    public static void replaceList(LinkedList<Object> list){
+        int n = list.size()/2;
+        int m = list.size();
+        for(int i = 0; i < n; i++){
+            Object temp = list.get(i);
+            list.set(i, list.get(m-i-1));
+            list.set(m-i-1, temp);
+        }
+    }
+    public static void main(String[] args) {
+        LinkedList<Object> listFirst = fillList();
+        System.out.println();
+        printList(listFirst);
+        replaceList(listFirst);
+        System.out.println();
+        printList(listFirst);
+        System.out.println("\n");
     }
 }
